bab3f921e27ea69c76a6a4040121c58562856b07
Komako <95164494+yelvtiiiiiii@users.noreply.github.com>
2022-12-19 10:58:59 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/「12·9合唱比賽」2022 QS', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/「12·9合唱比賽」2022 QS', '/Never Gonna Give You Up'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:owo@Komako.moe',
